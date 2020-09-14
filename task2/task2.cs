73c3e574f691a4fa97a919569c74c812af4081c4
dr3amy <arterterone@gmail.com>
2020-09-14 14:49:55 +0300

    init commit

---
+++ task2/task2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace color_spaces.task2
+{
+    public partial class task2 : Form
+    {
+        public task2()
+        {
+            InitializeComponent();
+        }
+    }
+}
