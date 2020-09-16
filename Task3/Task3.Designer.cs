cc7b7bd730fdc28824cdb81599a22ed5568920b4
stas1f1 <stas1f1@gmail.com>
2020-09-16 03:06:32 +0300

    Task 3 done

--- Task3/Task3.Designer.cs
+++ Task3/Task3.Designer.cs
@@ -28,12 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sourcePictureBox = new System.Windows.Forms.PictureBox();
+            this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.hueTrackBar = new System.Windows.Forms.TrackBar();
+            this.saturationTrackBar = new System.Windows.Forms.TrackBar();
+            this.valueTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveResultButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hueTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturationTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sourcePictureBox
+            // 
+            this.sourcePictureBox.Location = new System.Drawing.Point(12, 51);
+            this.sourcePictureBox.Name = "sourcePictureBox";
+            this.sourcePictureBox.Size = new System.Drawing.Size(352, 303);
+            this.sourcePictureBox.TabIndex = 2;
+            this.sourcePictureBox.TabStop = false;
+            // 
+            // resultPictureBox
+            // 
+            this.resultPictureBox.Location = new System.Drawing.Point(372, 51);
+            this.resultPictureBox.Name = "resultPictureBox";
+            this.resultPictureBox.Size = new System.Drawing.Size(352, 303);
+            this.resultPictureBox.TabIndex = 3;
+            this.resultPictureBox.TabStop = false;
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chooseFileButton.Location = new System.Drawing.Point(12, 15);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(91, 30);
+            this.chooseFileButton.TabIndex = 4;
+            this.chooseFileButton.Text = "choose file";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // hueTrackBar
+            // 
+            this.hueTrackBar.Location = new System.Drawing.Point(175, 372);
+            this.hueTrackBar.Maximum = 360;
+            this.hueTrackBar.Name = "hueTrackBar";
+            this.hueTrackBar.Size = new System.Drawing.Size(384, 45);
+            this.hueTrackBar.TabIndex = 5;
+            this.hueTrackBar.Scroll += new System.EventHandler(this.hueTrackBar_Scroll);
+            // 
+            // saturationTrackBar
+            // 
+            this.saturationTrackBar.Location = new System.Drawing.Point(175, 423);
+            this.saturationTrackBar.Maximum = 100;
+            this.saturationTrackBar.Name = "saturationTrackBar";
+            this.saturationTrackBar.Size = new System.Drawing.Size(384, 45);
+            this.saturationTrackBar.TabIndex = 6;
+            this.saturationTrackBar.Scroll += new System.EventHandler(this.saturationTrackBar_Scroll);
+            // 
+            // valueTrackBar
+            // 
+            this.valueTrackBar.Location = new System.Drawing.Point(175, 474);
+            this.valueTrackBar.Maximum = 100;
+            this.valueTrackBar.Name = "valueTrackBar";
+            this.valueTrackBar.Size = new System.Drawing.Size(384, 45);
+            this.valueTrackBar.TabIndex = 7;
+            this.valueTrackBar.Scroll += new System.EventHandler(this.valueTrackBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Hue";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 407);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Saturation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 455);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Value";
+            // 
+            // saveResultButton
+            // 
+            this.saveResultButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveResultButton.Location = new System.Drawing.Point(633, 15);
+            this.saveResultButton.Name = "saveResultButton";
+            this.saveResultButton.Size = new System.Drawing.Size(91, 30);
+            this.saveResultButton.TabIndex = 11;
+            this.saveResultButton.Text = "save result";
+            this.saveResultButton.UseVisualStyleBackColor = true;
+            this.saveResultButton.Click += new System.EventHandler(this.saveResultButton_Click);
+            // 
+            // Task3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(736, 575);
+            this.Controls.Add(this.saveResultButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valueTrackBar);
+            this.Controls.Add(this.saturationTrackBar);
+            this.Controls.Add(this.hueTrackBar);
+            this.Controls.Add(this.chooseFileButton);
+            this.Controls.Add(this.resultPictureBox);
+            this.Controls.Add(this.sourcePictureBox);
+            this.Name = "Task3";
             this.Text = "Task3";
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hueTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturationTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox sourcePictureBox;
+        private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar hueTrackBar;
+        private System.Windows.Forms.TrackBar saturationTrackBar;
+        private System.Windows.Forms.TrackBar valueTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button saveResultButton;
     }
 }
